db4ec8b55c1cc95670e6de689c815a6914ef42d1
21givenchy <65071041+21givenchy@users.noreply.github.com>
2023-06-23 15:20:39 +0300

    Update sdg-form.tsx

--- src/pages/sdg-form.tsx
+++ src/pages/sdg-form.tsx
@@ -1,64 +1,53 @@
-import { useState } from "react";
-import { Configuration, OpenAIApi } from "openai";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const configuration = new Configuration({apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+const SDGForm = () => {
+  const [education, setEducation] = useState('');
+  const [passions, setPassions] = useState('');
+  const [skills, setSkills] = useState('');
+  const [sdgs, setSDGs] = useState('');
 
-const App = () => {
-  const [education, setEducation] = useState("");
-  const [passion, setPassion] = useState("");
-  const [skills, setSkills] = useState("");
-
-  const handleSubmit = async () => {
-    const chatCompletion = await openai.createChatCompletion(
-      {
-        model: "gpt-3.5-turbo",
-        messages: [{role:"user",content:"Suggest SDGs that I can impact most based on my education, passion, and skills"}],
-      },
-      {
-        timeout: 1000,
-        headers: {
-          "climate": "change",
-        },
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      prompt: `What are the SDGs one can impact most through their education: ${education}, passions: ${passions}, and skills: ${skills}?`,
+      max_tokens: 60,
+      n: 1,
+      stop: ['\n']
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
       }
-      
-    );
-
-    const sdgs = Response.data.choices[0].message;
-
-    // Display the suggested SDGs
-    alert(sdgs);
-  
-  
+    });
+    setSDGs(response.data.choices[0].text);
   };
 
   return (
-    <div>
-      <h1>frontforumfocus</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Education"
-          value={education}
-          onChange={(e) => setEducation(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Passion"
-          value={passion}
-          onChange={(e) => setPassion(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Skills"
-          value={skills}
-          onChange={(e) => setSkills(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Education:
+        <input type="text" value={education} onChange={(event) => setEducation(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Passions:
+        <input type="text" value={passions} onChange={(event) => setPassions(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Skills:
+        <input type="text" value={skills} onChange={(event) => setSkills(event.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+      <br />
+      <label>
+        SDGs:
+        <textarea value={sdgs} readOnly />
+      </label>
+    </form>
   );
 };
 
-export default App;
+export default SDGForm;
